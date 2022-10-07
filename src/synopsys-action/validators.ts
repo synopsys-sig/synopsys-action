3dded815a3b2a8f1e7100532b8f640446100e726
lokesha <lokesha@synopsys.com>
2022-10-07 12:32:16 +0530

    feat: SIGINT-276 run all scans at once

--- src/synopsys-action/validators.ts
+++ src/synopsys-action/validators.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs'
 import {info} from '@actions/core'
 export function validatePolarisParams(accessToken: string, applicationName: string, projectName: string, serverURL: string, assessmentTypes: string[]): boolean {
   if (accessToken == null || accessToken.length === 0 || applicationName == null || applicationName.length === 0 || projectName == null || projectName.length === 0 || serverURL == null || serverURL.length === 0 || assessmentTypes.length === 0) {
-    info('One or more required parameters for Altair is missing')
+    info('One or more required parameters for Altair is missing, hence skipping Altair')
     return false
   }
   return true
@@ -10,7 +10,7 @@ export function validatePolarisParams(accessToken: string, applicationName: stri
 
 export function validateCoverityParams(userName: string, passWord: string, coverityUrl: string, projectName: string, streamName: string): boolean {
   if (userName == null || userName.length === 0 || passWord == null || passWord.length === 0 || coverityUrl == null || coverityUrl.length === 0 || projectName == null || projectName.length === 0 || streamName == null || streamName.length === 0) {
-    info('One or more required parameters for Coverity is missing')
+    info('One or more required parameters for Coverity is missing, hence skipping Coverity')
     return false
   }
   return true
@@ -18,11 +18,11 @@ export function validateCoverityParams(userName: string, passWord: string, cover
 
 export function validateCoverityInstallDirectoryParam(installDir: string): boolean {
   if (installDir == null || installDir.length === 0) {
-    info('One or more required parameters for Coverity is missing')
+    info('One or more required parameters for Coverity is missing, hence skipping Coverity')
     return false
   }
   if (!fs.existsSync(installDir)) {
-    info('Invalid Install Directory')
+    info('Invalid Install Directory, hence skipping Coverity')
     return false
   }
   return true
@@ -30,7 +30,7 @@ export function validateCoverityInstallDirectoryParam(installDir: string): boole
 
 export function validateBalckduckParams(url: string, apiToken: string): boolean {
   if (url == null || url.length === 0 || apiToken == null || apiToken.length === 0) {
-    info('One or more required parameters for BlackDuck is missing')
+    info('One or more required parameters for BlackDuck is missing, hence skipping BlackDuck')
     return false
   }
   return true
