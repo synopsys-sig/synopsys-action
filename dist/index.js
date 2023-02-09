2984a1aa874ea867a89be5420c0cafdf26c3585b
integration <kirann@synopsys.com>
2023-02-09 22:36:26 +0530

    SIGINT-553: assessment types.

--- dist/index.js
+++ dist/index.js
@@ -693,8 +693,8 @@ class SynopsysToolsParameter {
                 // converting provided assessmentTypes to uppercase
                 const assessmentTypes = assessmentTypesValues.toUpperCase().split(',');
                 for (const assessmentType of assessmentTypes) {
-                    const regExpression = new RegExp('^[0-9a-zA-Z]+$');
-                    if (assessmentType.trim().length > 0 && regExpression.test(assessmentType.trim())) {
+                    const regExp = new RegExp('^[0-9a-zA-Z]+$');
+                    if (assessmentType.trim().length > 0 && regExp.test(assessmentType.trim())) {
                         assessmentTypeEnums.push(polaris_1.PolarisAssessmentType[assessmentType.trim()]);
                     }
                 }
