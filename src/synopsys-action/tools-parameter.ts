e68454bd10ab65c0609474824b43f3600a242f5a
integration <kirann@synopsys.com>
2023-02-09 22:07:54 +0530

    SIGINT-553: assessment types.

--- src/synopsys-action/tools-parameter.ts
+++ src/synopsys-action/tools-parameter.ts
@@ -36,7 +36,8 @@ export class SynopsysToolsParameter {
         // converting provided assessmentTypes to uppercase
         const assessmentTypes = assessmentTypesValues.toUpperCase().split(',')
         for (const assessmentType of assessmentTypes) {
-          if (assessmentType.trim().length > 0) {
+          const regEx = new RegExp('^[0-9a-zA-Z]+$')
+          if (assessmentType.trim().length > 0 && regEx.test(assessmentType.trim())) {
             assessmentTypeEnums.push(PolarisAssessmentType[assessmentType.trim() as keyof typeof PolarisAssessmentType])
           }
         }
