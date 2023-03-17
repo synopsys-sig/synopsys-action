814ff70af9bbe791427e6a3d05827c16770ebc0c
lokesh <lokesha@synopsys.com>
2023-03-17 18:48:13 +0530

    feat: SIGINT-655 and SIGINT-655

--- test/contract/coverity.e2e.test.ts
+++ test/contract/coverity.e2e.test.ts
@@ -144,4 +144,9 @@ export function getBridgeDownloadUrl(): string {
 export function mockBridgeDownloadUrlAndSynopsysBridgePath() {
   Object.defineProperty(inputs, 'BRIDGE_DOWNLOAD_URL', {value: getBridgeDownloadUrl()})
   Object.defineProperty(inputs, 'SYNOPSYS_BRIDGE_PATH', {value: __dirname})
+  process.env['GITHUB_TOKEN'] = 'token'
+  process.env['GITHUB_REPOSITORY'] = 'synopsys-action'
+  process.env['GITHUB_HEAD_REF'] = 'branch-name'
+  process.env['GITHUB_REF'] = 'refs/pull/1/merge'
+  process.env['GITHUB_REPOSITORY_OWNER'] = 'synopsys-sig'
 }
