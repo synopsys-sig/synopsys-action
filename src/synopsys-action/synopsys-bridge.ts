752e0c53cc9fa4fe1f4cc0ecc9f7c5eb1d11bbff
Kiran Narayana <kirann@synopsys.com>
2023-08-08 16:45:58 +0530

    reading versions.txt from the specified downaload url

--- src/synopsys-action/synopsys-bridge.ts
+++ src/synopsys-action/synopsys-bridge.ts
@@ -93,19 +93,18 @@ export class SynopsysBridge {
   }
 
   async downloadBridge(tempDir: string): Promise<void> {
-    const LATEST = 'latest'
     try {
       // Automatically configure bridge if Bridge download url is provided
       let bridgeUrl = ''
       let bridgeVersion = ''
       if (inputs.BRIDGE_DOWNLOAD_URL) {
         bridgeUrl = BRIDGE_DOWNLOAD_URL
-        if (bridgeUrl.includes(LATEST)) {
-          bridgeVersion = await this.getSynopsysBridgeVersionFromLatestURL(bridgeUrl.substring(0, bridgeUrl.lastIndexOf(LATEST) + LATEST.length).concat('/versions.txt'))
-        } else {
-          const versionInfo = bridgeUrl.match('.*synopsys-bridge-([0-9.]*).*')
-          if (versionInfo != null) {
-            bridgeVersion = versionInfo[1]
+        const versionInfo = bridgeUrl.match('.*synopsys-bridge-([0-9.]*).*')
+        if (versionInfo != null) {
+          bridgeVersion = versionInfo[1]
+          if (!bridgeVersion) {
+            const regex = /\w*(synopsys-bridge-(win64|linux64|macosx).zip)/
+            bridgeVersion = await this.getSynopsysBridgeVersionFromLatestURL(bridgeUrl.replace(regex, 'versions.txt'))
           }
         }
       } else if (inputs.BRIDGE_DOWNLOAD_VERSION) {
