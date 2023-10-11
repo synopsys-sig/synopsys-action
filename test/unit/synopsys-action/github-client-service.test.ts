4255bf4c23610e12646cf2baeb0a0d2827084b1c
spurohit <spurohit@synopsys.com>
2023-10-11 11:58:41 +0530

    sarif upload test case correction

--- test/unit/synopsys-action/github-client-service.test.ts
+++ test/unit/synopsys-action/github-client-service.test.ts
@@ -6,9 +6,9 @@ import {GithubClientService} from '../../../src/synopsys-action/github-client-se
 import {Socket} from 'net'
 import * as artifact from '@actions/artifact'
 import * as configVariables from '@actions/artifact/lib/internal/config-variables'
-import any = jasmine.any
-import {UploadResponse} from '@actions/artifact'
+import {getWorkSpaceDirectory} from '@actions/artifact/lib/internal/config-variables'
 
+jest.mock('@actions/artifact')
 const originalEnv = process.env
 beforeEach(() => {
   jest.resetModules()
@@ -23,43 +23,42 @@ beforeEach(() => {
   Object.defineProperty(process, 'platform', {value: 'linux'})
 })
 
-test('should upload sarif report successfully', () => {
-  const githubClientService = new GithubClientService()
+describe('upload sarif', () => {
+  it('should upload sarif report successfully', async function () {
+    const githubClientService = new GithubClientService()
 
-  Object.defineProperty(inputs, 'GITHUB_TOKEN', {value: 'test-token'})
-  const incomingMessage: IncomingMessage = new IncomingMessage(new Socket())
-  const httpResponse: Mocked<HttpClientResponse> = {
-    message: incomingMessage,
-    readBody: jest.fn()
-  }
-  httpResponse.message.statusCode = 202
-  jest.spyOn(HttpClient.prototype, 'post').mockResolvedValueOnce(httpResponse)
+    Object.defineProperty(inputs, 'GITHUB_TOKEN', {value: 'test-token'})
+    const incomingMessage: IncomingMessage = new IncomingMessage(new Socket())
+    const httpResponse: Mocked<HttpClientResponse> = {
+      message: incomingMessage,
+      readBody: jest.fn()
+    }
+    httpResponse.message.statusCode = 202
+    jest.spyOn(HttpClient.prototype, 'post').mockResolvedValueOnce(httpResponse)
 
-  const mockUploadArtifact = jest.fn()
-  const mockArtifactClient: Partial<artifact.ArtifactClient> = {
-    uploadArtifact: mockUploadArtifact
-  }
-  const mockCreate = jest.spyOn(artifact, 'create').mockReturnValue(mockArtifactClient as artifact.ArtifactClient)
-  const mockDir = jest.spyOn(configVariables, 'getWorkSpaceDirectory').mockReturnValue('.')
-  const mockPwd = './.bridge/SARIF Generator'
-  const mockFiles = ['./.bridge/SARIF Generator/sarif_report.json']
-  //jest.spyOn(mockArtifactClient, 'uploadArtifact')
-  const uploadResponse = new Promise<UploadResponse>((resolve, reject) => {})
-  jest.spyOn(mockArtifactClient, 'uploadArtifact').mockReturnValue(uploadResponse)
+    const mockUploadArtifact = jest.fn()
+    const mockArtifactClient: Partial<artifact.ArtifactClient> = {
+      uploadArtifact: mockUploadArtifact
+    }
+    const mockCreate = jest.spyOn(artifact, 'create').mockReturnValue(mockArtifactClient as artifact.ArtifactClient)
+    jest.spyOn(configVariables, 'getWorkSpaceDirectory').mockReturnValue('.')
+    jest.spyOn(mockArtifactClient, 'uploadArtifact')
 
-  const response = githubClientService.uploadSarifReport()
-  //expect(mockUploadArtifact).toHaveBeenCalled()
-  expect(response).resolves.toBeUndefined()
-  mockCreate.mockRestore()
-})
+    const response = await githubClientService.uploadSarifReport()
+    expect(mockUploadArtifact).toHaveBeenCalled()
+    expect(mockUploadArtifact).toHaveBeenCalledWith('sarif_report', ['./.bridge/SARIF Generator/sarif_report.json'], './.bridge/SARIF Generator', {continueOnError: false})
+    expect(response).toBeUndefined()
+    mockCreate.mockRestore()
+  })
 
-test('should not upload sarif report if it does not exist', () => {
-  const githubClientService = new GithubClientService()
+  it('should not upload sarif report if it does not exist', async function () {
+    const githubClientService = new GithubClientService()
 
-  Object.defineProperty(inputs, 'GITHUB_TOKEN', {value: 'test-token'})
-  Object.defineProperty(inputs, 'REPORTS_SARIF_FILE_PATH', {value: 'test' + ''})
-  const response = githubClientService.uploadSarifReport()
-  expect(response).resolves.toBeUndefined()
+    Object.defineProperty(inputs, 'GITHUB_TOKEN', {value: 'test-token'})
+    Object.defineProperty(inputs, 'REPORTS_SARIF_FILE_PATH', {value: 'test' + ''})
+    const response = await githubClientService.uploadSarifReport()
+    expect(response).toBeUndefined()
+  })
 })
 
 afterEach(() => {
