cbdc1d19b091347dbf0fbd318ef5b73f4530e1d6
Sadman Anik <sadman.anik@dsinnovators.com>
2023-12-07 16:06:04 +0600

    Defaulting github.host.url from GITHUB_SERVER_URL environment variable

--- src/synopsys-action/tools-parameter.ts
+++ src/synopsys-action/tools-parameter.ts
@@ -288,7 +288,7 @@ export class SynopsysToolsParameter {
     const githubRepoName = githubRepo !== undefined ? githubRepo.substring(githubRepo.indexOf('/') + 1, githubRepo.length).trim() : ''
     const githubBranchName = parseToBoolean(inputs.POLARIS_PRCOMMENT_ENABLED) ? process.env[GITHUB_ENVIRONMENT_VARIABLES.GITHUB_HEAD_REF] : process.env[GITHUB_ENVIRONMENT_VARIABLES.GITHUB_REF_NAME]
     const githubRef = process.env[GITHUB_ENVIRONMENT_VARIABLES.GITHUB_REF]
-    const githubHostUrl = inputs.GITHUB_HOST_URL ? inputs.GITHUB_HOST_URL : ''
+    const githubHostUrl = inputs.GITHUB_HOST_URL ? inputs.GITHUB_HOST_URL : process.env[GITHUB_ENVIRONMENT_VARIABLES.GITHUB_SERVER_URL]
 
     // pr number will be part of "refs/pull/<pr_number>/merge"
     // if there is manual run without raising pr then GITHUB_REF will return refs/heads/branch_name
@@ -304,7 +304,7 @@ export class SynopsysToolsParameter {
     }
 
     // This condition is required as per ts-lint as these fields may have undefined as well
-    if (githubRepoName != null && githubBranchName != null && githubRepoOwner != null) {
+    if (githubRepoName != null && githubBranchName != null && githubRepoOwner != null && githubHostUrl != null) {
       return this.setGithubData(githubToken, githubRepoName, githubRepoOwner, githubBranchName, githubPrNumber, githubHostUrl)
     }
     return undefined
