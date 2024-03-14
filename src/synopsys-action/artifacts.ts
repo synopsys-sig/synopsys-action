9634ac6883a024e1fbab7332b5fa72b96c8b25c6
spurohit <spurohit@synopsys.com>
2024-03-14 14:26:19 +0530

    handle artifact upload error

--- src/synopsys-action/artifacts.ts
+++ src/synopsys-action/artifacts.ts
@@ -52,10 +52,15 @@ export function getFiles(dir: string, allFiles: string[]): string[] {
 }
 
 export async function uploadSarifReportAsArtifact(defaultSarifReportDirectory: string, userSarifFilePath: string, artifactName: string): Promise<UploadResponse> {
-  const artifactClient = artifact.create()
-  const sarifFilePath = userSarifFilePath ? userSarifFilePath : getDefaultSarifReportPath(defaultSarifReportDirectory, true)
-  const rootDir = userSarifFilePath ? path.dirname(userSarifFilePath) : getDefaultSarifReportPath(defaultSarifReportDirectory, false)
-  const options: UploadOptions = {}
-  options.continueOnError = false
-  return await artifactClient.uploadArtifact(artifactName, [sarifFilePath], rootDir, options)
+  try {
+    const artifactClient = artifact.create()
+    const sarifFilePath = userSarifFilePath ? userSarifFilePath : getDefaultSarifReportPath(defaultSarifReportDirectory, true)
+    const rootDir = userSarifFilePath ? path.dirname(userSarifFilePath) : getDefaultSarifReportPath(defaultSarifReportDirectory, false)
+    const options: UploadOptions = {}
+    options.continueOnError = false
+    return await artifactClient.uploadArtifact(artifactName, [sarifFilePath], rootDir, options)
+  } catch (error) {
+    warning(`Error occurred while uploading SARIF report as artifact: ${error}`)
+    return Promise.reject(error)
+  }
 }
