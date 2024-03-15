838bd835a04816fbec065775f0191d75d9fd7ee9
spurohitsynopsys <116278347+spurohitsynopsys@users.noreply.github.com>
2024-03-15 17:08:27 +0530

    Ignore BD or Polaris SARIF flags in case of PR context (#210)

--- test/unit/synopsys-action/utility.test.ts
+++ test/unit/synopsys-action/utility.test.ts
@@ -1,4 +1,5 @@
-import {cleanUrl, isBoolean} from '../../../src/synopsys-action/utility'
+import {cleanUrl, isBoolean, isPullRequestEvent} from '../../../src/synopsys-action/utility'
+import * as constants from '../../../src/application-constants'
 test('cleanUrl() trailing slash', () => {
   const validUrl = 'https://my-domain.com'
   const testUrl = `${validUrl}/`
@@ -35,6 +36,36 @@ describe('isBoolean', () => {
 
   it('should return false with any random string value', function () {
     const result = isBoolean('test')
+    expect(result).toEqual(false)
+  })
+})
+
+describe('isPullRequestEvent', () => {
+  let originalEventName: string
+
+  beforeEach(() => {
+    originalEventName = process.env[constants.GITHUB_ENVIRONMENT_VARIABLES.GITHUB_EVENT_NAME] || ''
+  })
+
+  afterEach(() => {
+    process.env[constants.GITHUB_ENVIRONMENT_VARIABLES.GITHUB_EVENT_NAME] = originalEventName
+  })
+
+  it('should return true if event name is pull_request', () => {
+    process.env[constants.GITHUB_ENVIRONMENT_VARIABLES.GITHUB_EVENT_NAME] = 'pull_request'
+    const result = isPullRequestEvent()
+    expect(result).toEqual(true)
+  })
+
+  it('should return false if event name is not pull_request', () => {
+    process.env[constants.GITHUB_ENVIRONMENT_VARIABLES.GITHUB_EVENT_NAME] = 'push'
+    const result = isPullRequestEvent()
+    expect(result).toEqual(false)
+  })
+
+  it('should return false if event name is undefined', () => {
+    process.env[constants.GITHUB_ENVIRONMENT_VARIABLES.GITHUB_EVENT_NAME] = undefined
+    const result = isPullRequestEvent()
     expect(result).toEqual(false)
   })
 })
